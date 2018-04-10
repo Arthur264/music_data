fce3c3387a6213e2f7f56d914c7953d68c313c73
Artyr <artyr2643@gmail.com>
2018-04-10 22:19:43 +0000

    add sms

---
+++ sms/client.py
@@ -0,0 +1,9 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "AC8da189db4309a784c0575fa107f3b665"
+# Your Auth Token from twilio.com/console
+auth_token  = "37676f06d89cf7d51010420db640cab5"
+
+client = Client(account_sid, auth_token)
+
