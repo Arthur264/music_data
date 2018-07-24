c0637ddee2fdb6a005c7bc5356508ef748db4129
Arthur Feduniv <afeduniv@lvivsoft.com>
2018-07-24 13:12:42 +0300

    add new zk file

--- music/music/pipelines.py
+++ music/music/pipelines.py
@@ -5,38 +5,68 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
-
+import time
 from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
+from scrapy.exporters import JsonItemExporter
 
 
 class MusicPipeline(object):
+    current_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+
     def __init__(self):
-        self.connection = pymongo.MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
-        )
-        db = self.connection[settings['MONGODB_DB']]
-        self.item = db[settings['MONGODB_COLLECTION_ITEM']]
-        self.artict = db[settings['MONGODB_COLLECTION_ARTICT']]
+        self.file_item = open("music_{}.json".format(self.current_time), 'wb')
+        self.item = JsonItemExporter(self.file_item, encoding='utf-8', ensure_ascii=False)
+        self.item.start_exporting()
+        self.file_artist = open("artist_{}.json".format(self.current_time), 'wb')
+        self.artist = JsonItemExporter(self.file_artist, encoding='utf-8', ensure_ascii=False)
+        self.artist.start_exporting()
 
     def close_spider(self, spider):
-            self.connection.close()
+        self.item.finish_exporting()
+        self.item.close()
+
+        self.artist.finish_exporting()
+        self.artist.close()
 
     def process_item(self, item, spider):
-        valid = True
-        for data in item:
-            if not data:
-                valid = False
-                raise DropItem("Missing {0}!".format(data))
-        if valid:
-            if item.__class__.__name__ == "MusicItem":
-                self.item.insert(dict(item))
-                log.msg("Item added to MongoDB database!",
+        if item.__class__.__name__ == "MusicItem":
+            self.item.export_item(item)
+            log.msg("Item added to MongoDB database!",
                     level=log.DEBUG, spider=spider)
-            else:
-                self.artict.insert(dict(item))
-                log.msg("Artist added to MongoDB database!",
-                        level=log.DEBUG, spider=spider)
+        else:
+            self.artist.export_item(item)
+            log.msg("Artist added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
         return item
+
+# class MusicPipeline(object):
+#     def __init__(self):
+#         self.connection = pymongo.MongoClient(
+#             settings['MONGODB_SERVER'],
+#             settings['MONGODB_PORT']
+#         )
+#         db = self.connection[settings['MONGODB_DB']]
+#         self.item = db[settings['MONGODB_COLLECTION_ITEM']]
+#         self.artict = db[settings['MONGODB_COLLECTION_ARTICT']]
+#
+#     def close_spider(self, spider):
+#             self.connection.close()
+#
+#     def process_item(self, item, spider):
+#         valid = True
+#         for data in item:
+#             if not data:
+#                 valid = False
+#                 raise DropItem("Missing {0}!".format(data))
+#         if valid:
+#             if item.__class__.__name__ == "MusicItem":
+#                 self.item.insert(dict(item))
+#                 log.msg("Item added to MongoDB database!",
+#                     level=log.DEBUG, spider=spider)
+#             else:
+#                 self.artict.insert(dict(item))
+#                 log.msg("Artist added to MongoDB database!",
+#                         level=log.DEBUG, spider=spider)
+#         return item
--- music/music/spiders/zk.py
+++ music/music/spiders/zk.py
@@ -8,7 +8,7 @@ from music.items import MusicItem, ArtistItem
 
 BASE_URL = 'http://zk.fm'
 DEFAULT_ARTIST = 'http://www.collectionsocietegenerale.com/data/artiste_a0e33/fiche/4733/large_large_artist_generique_2e4c9.gif'
-HEADERS= {
+HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9,uk;q=0.8',
@@ -22,32 +22,38 @@ HEADERS2 = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.3'
 }
 
+
 class ZkSpider(scrapy.Spider):
     name = 'music'
     allowed_domains = ['zk.fm', 'www.google.com']
     handle_httpstatus_list = [304]
+
     def start_requests(self):
-        for n in range(60, 200):
-            print("Count:", n)
+        for n in range(91294, 100000):
+            # print("Count:", n)
             yield scrapy.Request(url=BASE_URL + '/artist/' + str(n),
-                                        meta={'atrict': n, 'handle_httpstatus_all': True, "dont_merge_cookie": True},
-                                        errback=self.error,
-                                        headers=HEADERS2,
-                                        callback=self.parse)
+                                 meta={'atrict': n, 'handle_httpstatus_all': True, "dont_merge_cookie": True},
+                                 errback=self.error,
+                                 headers=HEADERS2,
+                                 callback=self.parse)
 
     def parse(self, response):
         print("url", response.request.url, response.status)
+        if response.status in [404]:
+            return
         title_selector = response.css("#container .title_box h1::text").extract_first()
         if not title_selector:
             return
-        title = self.text_encode(title_selector.rstrip().strip())
+        title = title_selector.rstrip().strip()
         if not title:
             return
-        params={'q': title + '+group', 'source': 'lnms', 'tbm': 'isch', 'safe':'active'}
-        yield scrapy.Request("https://www.google.com/search?" + urlencode(params),
-                    callback =self.getlink,
-                    meta={"name": title,'artict': response.meta['atrict'], 'proxy': self.get_proxy()},
-                    headers=HEADERS)
+        # print(response.meta['atrict'], title, DEFAULT_ARTIST)
+        yield ArtistItem(id=response.meta['atrict'], name=title, image=DEFAULT_ARTIST)
+        # params={'q': title + '+group', 'source': 'lnms', 'tbm': 'isch', 'safe':'active'}
+        # yield scrapy.Request("https://www.google.com/search?" + urlencode(params),
+        #             callback =self.getlink,
+        #             meta={"name": title,'artict': response.meta['atrict'], 'proxy': self.get_proxy()},
+        #             headers=HEADERS)
 
         for r in self.get_items(response):
             yield r
@@ -61,24 +67,22 @@ class ZkSpider(scrapy.Spider):
                 if not song_name:
                     continue
                 song_link = self.get_url(item.css("span.song-download").xpath('@data-url').extract_first())
-            except AttributeError:
+            except AttributeError as e:
                 continue
-
+            # print(atrict, song_time, song_name, song_link)
             yield MusicItem(artict=atrict, time=song_time, name=song_name, url=song_link)
 
         next_page = response.css("a.next-btn")
         if next_page and 'disabled' not in next_page.xpath("@class").extract_first():
             yield scrapy.Request(self.get_url(next_page.xpath("@href").extract_first()),
-                                meta={"atrict": atrict, "dont_merge_cookie": True},
-                                headers=HEADERS2,
-                                callback=self.get_items)
-
-
+                                 meta={"atrict": atrict, "dont_merge_cookie": True},
+                                 headers=HEADERS2,
+                                 callback=self.get_items)
 
     def getlink(self, response):
         a = response.css("div.rg_meta::text").extract_first()
         try:
-            link =json.loads(a)["ou"]
+            link = json.loads(a)["ou"]
         except:
             link = DEFAULT_ARTIST
         yield ArtistItem(id=response.meta['artict'], name=response.meta['name'], image=link)
@@ -107,7 +111,7 @@ class ZkSpider(scrapy.Spider):
                 result.append(item[0])
         return result
 
-    def error(e,response):
+    def error(e, response):
         # import pdb; pdb.set_trace()
         print("Error:", e, response)
         return True
