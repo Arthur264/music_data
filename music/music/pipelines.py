4037ec7c23cf963f770abb54b04b83302375e04f
Arthur Feduniv <afeduniv@lvivsoft.com>
2018-07-25 13:55:31 +0300

    add utils

--- music/music/pipelines.py
+++ music/music/pipelines.py
@@ -40,33 +40,34 @@ class MusicPipeline(object):
             log.msg("Artist added to MongoDB database!",
                     level=log.DEBUG, spider=spider)
         return item
+        
 
-# class MusicPipeline(object):
-#     def __init__(self):
-#         self.connection = pymongo.MongoClient(
-#             settings['MONGODB_SERVER'],
-#             settings['MONGODB_PORT']
-#         )
-#         db = self.connection[settings['MONGODB_DB']]
-#         self.item = db[settings['MONGODB_COLLECTION_ITEM']]
-#         self.artict = db[settings['MONGODB_COLLECTION_ARTICT']]
-#
-#     def close_spider(self, spider):
-#             self.connection.close()
-#
-#     def process_item(self, item, spider):
-#         valid = True
-#         for data in item:
-#             if not data:
-#                 valid = False
-#                 raise DropItem("Missing {0}!".format(data))
-#         if valid:
-#             if item.__class__.__name__ == "MusicItem":
-#                 self.item.insert(dict(item))
-#                 log.msg("Item added to MongoDB database!",
-#                     level=log.DEBUG, spider=spider)
-#             else:
-#                 self.artict.insert(dict(item))
-#                 log.msg("Artist added to MongoDB database!",
-#                         level=log.DEBUG, spider=spider)
-#         return item
+class MusicMongoPipeline(object):
+    def __init__(self):
+        self.connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = self.connection[settings['MONGODB_DB']]
+        self.item = db[settings['MONGODB_COLLECTION_ITEM']]
+        self.artict = db[settings['MONGODB_COLLECTION_ARTICT']]
+
+    def close_spider(self, spider):
+            self.connection.close()
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            if item.__class__.__name__ == "MusicItem":
+                self.item.insert(dict(item))
+                log.msg("Item added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
+            else:
+                self.artict.insert(dict(item))
+                log.msg("Artist added to MongoDB database!",
+                        level=log.DEBUG, spider=spider)
+        return item
---
+++ music/music/utils/heders.py
@@ -0,0 +1,6 @@
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,uk;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.3'
+}
